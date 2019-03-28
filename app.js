8e937088411852952e86b71f65fbd6370a2dfcc9
Patel <a_patel73563@tss-hr420-dm27.fc.ca>
2019-03-28 16:51:47 -0400

    chat app

--- app.js
+++ app.js
@@ -20,3 +20,24 @@ app.get('/', (req, res, next) => {
 const server = app.listen(port, () => {
     console.log(`app is runing on port ${port}`);
 })
+
+//plug in  chat app package
+io.attach(server);
+
+io.on('connection', function(socket){
+    console.log('a user has connected');
+    socket.emit('connected', {sID: `${socket.id}`, message: 'new connection'} );
+
+    // listen for incoming message
+    socket.on('chat message', function(msg){
+        // check the message
+        console.log('message', msg, 'socket', socket.id);
+
+        // send a message to every connected client
+        io.emit('chat message', { id: `${socket.id}`, message: msg });
+    });
+    
+    socket.on('disconnect', function(){
+        console.log('a user has disconnected');
+    });
+});
